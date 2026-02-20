7dff0f97b1364e822ffc92a625c092ff9f072496
Anton <stakan993@gmail.com>
2026-02-20 20:18:22 +0800

    91

--- 91.decode-ways.cs
+++ 91.decode-ways.cs
@@ -5,10 +5,33 @@
  */
 
 // @lc code=start
-public class Solution {
-    public int NumDecodings(string s) {
-        
+using System.Net.NetworkInformation;
+
+public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        if (string.IsNullOrEmpty(s) || s[0] == '0') return 0;
+        var dp = new int[s.Length+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (var i = 2; i <= s.Length; i++)
+        {
+            ///101010101
+            if (s[i-1] != '0')
+            {
+                dp[i] += dp[i-1];
+            } 
+
+            var d2 = int.Parse(s.Substring(i-2, 2));
+            if (d2 >=10 && d2 <= 26)
+            {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.Length];
     }
+
 }
 // @lc code=end
 
---
+++ 92.reverse-linked-list-ii.cs
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=92 lang=csharp
+ *
+ * [92] Reverse Linked List II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseBetween(ListNode head, int left, int right) {
+        
+    }
+}
+// @lc code=end
+
